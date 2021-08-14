bfa6a58eb85b480634406b307bea2759a65ba7e4
niciki <71276784+niciki@users.noreply.github.com>
2021-08-14 15:00:56 +0300

    Create Sierpinski_square.py

---
+++ Sierpinski_square.py
@@ -0,0 +1,17 @@
+import random as R
+import turtle as T
+
+T.hideturtle()
+T.speed(0)
+vertex_coordinates = [[-200,-100], [-200, 300], [200,-100], [200, 300], [-200, 100], [0, -100], [0, 300], [200, 100]]
+x, y = vertex_coordinates[0]
+for i in range(100000):
+    r = R.randrange(8)
+    x_ = (x + 2 * vertex_coordinates[r][0]) / 3
+    y_ = (y + 2 * vertex_coordinates[r][1]) / 3
+    T.penup()
+    T.goto(x_, y_)
+    T.pendown()
+    x, y = x_, y_
+    T.dot(2)
+T.done()
