f654aebffc0c0d5a9a6bcef405f974d2cf36f280
niciki <71276784+niciki@users.noreply.github.com>
2021-08-14 15:56:40 +0300

    Update Sierpinski_square_turtle.py

--- Sierpinski_square_turtle.py
+++ Sierpinski_square_turtle.py
@@ -1,11 +1,13 @@
 import random as R
 import turtle as T
 
+n = 10000
+
 T.hideturtle()
 T.speed(0)
 vertex_coordinates = [[-200,-100], [-200, 300], [200,-100], [200, 300], [-200, 100], [0, -100], [0, 300], [200, 100]]
 x, y = vertex_coordinates[0]
-for i in range(100000):
+for i in range(n):
     r = R.randrange(8)
     x_ = (x + 2 * vertex_coordinates[r][0]) / 3
     y_ = (y + 2 * vertex_coordinates[r][1]) / 3
