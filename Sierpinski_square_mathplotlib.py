a60ee3f8eefb848cf083f4dca60fd9ae3dfaa34c
niciki <71276784+niciki@users.noreply.github.com>
2021-08-14 15:51:15 +0300

    Create Sierpinski_square_mathplotlib.py

---
+++ Sierpinski_square_mathplotlib.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import random as R
+
+n  = 1000000
+
+vertex_coordinates = [[-200,-100], [-200, 300], [200,-100], [200, 300], [-200, 100], [0, -100], [0, 300], [200, 100]]
+x_coordinates = [vertex_coordinates[0][0]]
+y_coordinates = [vertex_coordinates[0][1]]
+for i in range(n):
+    r = R.randrange(8)
+    x_coordinates.append((x_coordinates[-1] + 2 * vertex_coordinates[r][0]) / 3)
+    y_coordinates.append((y_coordinates[-1] + 2 * vertex_coordinates[r][1]) / 3)
+fig, ax = plt.subplots()
+ax.scatter(x_coordinates, y_coordinates, s = 0.1)
+ax.set(title='Случайные точки')
+plt.show()
